8a99b50b63d9263c0d2c81e743449db2d36a12f0
MaioCiranda <111889111+MaioCiranda@users.noreply.github.com>
2026-05-24 02:57:14 -0300

    Add pages router to main application

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import init_db
-from app.routers import auth, pontos
+from app.routers import auth, pontos, pages
 
 
 @asynccontextmanager
@@ -30,7 +30,7 @@ app.add_middleware(
 
 app.include_router(auth.router)
 app.include_router(pontos.router)
-
+app.include_router(pages.router)
 
 @app.get("/")
 def read_root():
